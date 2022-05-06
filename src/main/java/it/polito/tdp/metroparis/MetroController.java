0d811c243b207d7e3ba6eba7bf2d852afc1adedf
s271882@studenti.polito.it <s271882@studenti.polito.it>
2022-05-06 15:11:48 +0200

    Soluzione (parziale) branch

--- src/main/java/it/polito/tdp/metroparis/MetroController.java
+++ src/main/java/it/polito/tdp/metroparis/MetroController.java
@@ -1,5 +1,60 @@
 package it.polito.tdp.metroparis;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.metroparis.model.Fermata;
+import it.polito.tdp.metroparis.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class MetroController {
 	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Fermata> cmbArrivo;
+
+    @FXML
+    private ComboBox<Fermata> cmbPartenza;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void handleRicerca(ActionEvent event) {
+    	Fermata partenza = this.cmbPartenza.getValue();
+    	Fermata arrivo = this.cmbArrivo.getValue();
+    	if(partenza != null && arrivo!=null && !partenza.equals(arrivo)) {
+    		List<Fermata> percorso = this.model.calcolaPercorso(partenza, arrivo);
+    		this.txtResult.setText(percorso.toString());
+    	}else {
+    		this.txtResult.setText("Devi selezionare due stazioni diverse tra loro\n");
+    	}
+    }
+    
+    public void setModel(Model m) {
+    	this.model = m;
+    	List<Fermata> fermate = this.model.getFermate();
+    	this.cmbPartenza.getItems().addAll(fermate);
+    	this.cmbArrivo.getItems().addAll(fermate);
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbArrivo != null : "fx:id=\"cmbArrivo\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert cmbPartenza != null : "fx:id=\"cmbPartenza\" was not injected: check your FXML file 'Metro.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Metro.fxml'.";
+
+    }
+
 }
--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 import com.javadocmd.simplelatlng.LatLng;
 
 import it.polito.tdp.metroparis.model.Connessione;
+import it.polito.tdp.metroparis.model.CoppiaID;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -69,6 +70,83 @@ public class MetroDAO {
 		return linee;
 	}
 
+	public boolean isFermateConnesse(Fermata partenza, Fermata arrivo) {
+		String sql=" SELECT COUNT(*) AS cnt FROM connessione WHERE id_stazP=? AND id_stazA=? ";
+		Connection conn = DBConnect.getConnection();
+		try{
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			st.setInt(2, arrivo.getIdFermata());
+			ResultSet res = st.executeQuery();
+			res.first();
+			int count = res.getInt("cnt"); // == 0 oppure != 0
+			conn.close();
+			return count>0; //se sono connesse restituisce true, altrimenti false
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore database", e);
+		}
+	}
 	
+	public List<Integer> getIdFermateConnesse(Fermata partenza){
+		String sql = "SELECT id_stazA FROM connessione WHERE id_stazP=? GROUP BY id_stazA ";
+		Connection conn = DBConnect.getConnection();
+		try{
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			ResultSet res = st.executeQuery();
+			List<Integer> result = new ArrayList<Integer>();
+			while(res.next()) {
+				result.add(res.getInt("id_stazA"));
+			}
+			conn.close();
+			return result;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<Fermata> getFermateConnesse(Fermata partenza){
+		final String sql = "SELECT id_fermata, nome, coordx, coordy FROM fermata WHERE id_fermata IN (SELECT id_stazA FROM connessione WHERE id_stazP= ? GROUP BY id_stazA) ORDER BY nome ASC";
+		List<Fermata> fermate = new ArrayList<Fermata>();
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				Fermata f = new Fermata(rs.getInt("id_Fermata"), rs.getString("nome"),
+						new LatLng(rs.getDouble("coordx"), rs.getDouble("coordy")));
+				fermate.add(f);
+			}
+			st.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore di connessione al Database.");
+		}
+		return fermate;
+	}
+
+	public List<CoppiaID> getAllFermateConnesse(){
+		String sql = "SELECT DISTINCT id_stazA, id_stazP FROM connessione ";
+		List<CoppiaID> result = new ArrayList<CoppiaID>();
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				result.add(new CoppiaID(rs.getInt("id_stazP"),rs.getInt("id_stazA") ));
+			}
+			st.close();
+			conn.close();
+			return result;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}	
 
 }
